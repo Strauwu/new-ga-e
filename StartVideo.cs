a1749c6b9c91dc923ac5af7c645c9bff432ef2ea
Варвара <chastuxina.varya@mail.ru>
2024-05-22 15:56:08 +0500

    На главном меню добавлена кнопка выхода из игры. Теперь в игре 2 концовки: хорошая, при излечении всех жителей и плохая, при истечении времени. После того как игрок выиграет или проиграет снова откроется меню.

--- StartVideo.cs
+++ StartVideo.cs
@@ -4,10 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace new_ga_e
